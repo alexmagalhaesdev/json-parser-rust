ba0f10e71973698ad632572d7310bf01c9dbc3d3
alexmagalhaesdev <summer.magalhaes@gmail.com>
2024-05-23 17:17:37 -0300

    implement serialize and deserialize with rust

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use serde::{Deserialize, Serialize};
-use serde_json::to_string;
+use serde_json::{from_str, to_string_pretty};
 
-#[derive(Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
+#[derive(Serialize, Deserialize, Debug)]
 struct DogOwner {
     first_name: String,
     last_name: String,
 }
 
-#[derive(Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
 struct Dog {
     name: String,
     year_born: i32,
@@ -17,6 +16,11 @@ struct Dog {
 }
 
 fn main() {
+    serialize();
+    deserialize();
+}
+
+fn serialize() {
     let owner_1 = DogOwner {
         first_name: "Trevor".to_string(),
         last_name: "Sullivan".to_string(),
@@ -26,11 +30,32 @@ fn main() {
         year_born: 2021,
         owner: owner_1,
     };
-    let dog_ser = to_string(&dog_1);
+    let dog_ser: Result<String, serde_json::Error> = to_string_pretty(&dog_1);
 
     if dog_ser.is_ok() {
         println!("{}", dog_ser.ok().unwrap());
     } else {
         println!("{:#?}", dog_ser.err())
+    }
+}
+
+fn deserialize() {
+    let json_string: &str = r#"
+    {
+        "name": "Cheyenne",
+        "year_born": 2021,
+        "owner": {
+          "first_name": "Trevor",
+          "last_name": "Sullivan"
+        }
+      }
+    "#;
+
+    let dog_deser: Result<Dog, serde_json::Error> = from_str::<Dog>(json_string);
+
+    if dog_deser.is_ok() {
+        println!("{:#?}", dog_deser.ok().unwrap());
+    } else {
+        println!("{:#?}", dog_deser.err())
     }
 }
